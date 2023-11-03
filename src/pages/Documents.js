e5bbc8505517956730ec1850658e1a9ebfcf41c9
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2023-11-03 21:45:04 +0100

    document adresss changed

--- src/pages/Documents.js
+++ src/pages/Documents.js
@@ -6,7 +6,7 @@ const Documents = () => {
     return(
         <div className={clsx(styles.documents, " d-flex flex-column align-items-center p-4")}>
             <h2>Dokumenty do pobrania</h2>
-            <a href={`${process.env.PUBLIC_URL}/documents/Wniosek_o_skierowanie_do_zol.pdf`} target="_blank" rel='noreferrer'>Wniosek o skierowanie do ZOL</a>
+            <a href={`${process.env.PUBLIC_URL}/documents/Wniosek_o_skierowanie_do_ZOL.pdf`} target="_blank" rel='noreferrer'>Wniosek o skierowanie do ZOL</a>
             <a href={`${process.env.PUBLIC_URL}/documents/Wywiad_pielęgniarski_i_zaświadczenie_lekarskie.pdf`} target="_blank" rel='noreferrer'>Wywiad pielęgniarski i zaświadczenie lekarskie</a>
             <a href={`${process.env.PUBLIC_URL}/documents/Skierowanie_do_ZOL.pdf`} target="_blank" rel='noreferrer'>Skierowanie do ZOL</a>
             <a href={`${process.env.PUBLIC_URL}/documents/Skala_Barthel.pdf`} target="_blank" rel='noreferrer'>Skala Barthel</a>
